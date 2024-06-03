1e7137c51ba45f0787949fed307dc81c61f80678
Ruslan Abbas Al Faraui <rusya.abbas@gmail.com>
2024-06-03 14:42:44 +0200

    Formatted nmap processing

---
+++ netzhund.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+while read line; do
+  IFS=':' read -r IP PORT PROTOCOL SERVICE <<< "$line"
+  if [[ -z "$SERVICE" ]]; then SERVICE="unknown"; fi
+  mkdir -p "$SERVICE"
+  echo "$IP" >> "$SERVICE/hosts.txt"
+  echo "$IP:$PORT:$PROTOCOL" >> "$SERVICE/ports.txt"
+done < formatted-nmap.txt
+
+for dir in */; do
+  sort -o "$dir/hosts.txt" -u "$dir/hosts.txt"
+  sort -o "$dir/ports.txt" -u "$dir/ports.txt"
+done
