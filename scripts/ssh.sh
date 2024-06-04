e05473590e827ee51cde573c3677794af585b414
Ruslan Abbas Al Faraui <rusya.abbas@gmail.com>
2024-06-05 01:55:02 +0200

    ncat section finishes

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -2,15 +2,15 @@
 
 declare -a IPS PORTS
 
-########
-# ncat #
-########
 while read LINE; do
   IFS=':' read -r IP PORT PROTOCOL <<< "$LINE"
   IPS+=("$IP")
   PORTS+=("$PORT")
 done
 
+########
+# ncat #
+########
 for i in "${!IPS[@]}"; do
   echo "ncat --verbose --nodns ${IPS[i]} ${PORTS[i]}" >> ncat.txt
   ncat --verbose --nodns ${IPS[i]} ${PORTS[i]} >> ncat.txt 2>&1
@@ -18,3 +18,7 @@ done
 
 echo "$(ncat --version 2>&1)
 $(sed '/nmap.org/d; s/^/  /g; s/^  ncat/\nncat/g' ncat.txt)" > ncat.txt
+
+########
+# ???? #
+########
