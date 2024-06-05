f115e227c4964ee3de20274b38e62de86aa69bfe
EvardMellow <myshihka@gmail.com>
2024-06-05 10:36:58 +0300

    Added initial nmap scans, nmap parsing and creation of project directory structure

--- netzhund.sh
+++ netzhund.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+#Create file structure
+mkdir -p srv/{nmap,services}
+cd srv
+
+#Initial nmap scans
+nmap -sT -iL hosts.txt --top-ports=1000 -oA nmap/tcp -T2
+sudo nmap -sU -iL hosts.txt --top-ports=1000 -oA nmap/udp -T2
+
+#Create directories for services
+
 #date -I
 SCAN_NAME=LOL
 
@@ -16,5 +26,8 @@ for SERVICE in */; do
   sort -o "scans/$SCAN_NAME/$SERVICE/ports.txt" -u "scans/$SCAN_NAME/$SERVICE/ports.txt"
   cat "$SERVICE/ports.txt" | ./scripts/$SERVICE.sh &
 done
+
+#Nmap srv script
+nmap -sT -sU -sV -sC -iL hosts.txt -pT:$(cat ports-tcp.txt)U:$(cat ports-udp.txt) -oA nmap/all -T2
 
 wait
