3e2c6ee87d4913a9264ce06b136ba96c60f21fd5
faraui <rusya.abbas@gmail.com>
2024-06-05 15:33:43 +0200

    chameleon

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
+read OUTPUT_DIR
 declare -a IPS PORTS
-
 while read LINE; do
   IFS=':' read -r IP PORT PROTOCOL <<< "$LINE"
   IPS+=("$IP")
   PORTS+=("$PORT")
-done
+done < $OUTPUT_DIR/ports.txt
 
 ########
 # ncat #
 ########
 for i in "${!IPS[@]}"; do
-  echo "ncat --verbose --nodns ${IPS[i]} ${PORTS[i]}" >> ncat.txt
-  ncat --verbose --nodns ${IPS[i]} ${PORTS[i]} >> ncat.txt 2>&1
+  echo "ncat --verbose --nodns ${IPS[i]} ${PORTS[i]}" >> $OUTPUT_DIR/ncat.txt
+  ncat --verbose --nodns ${IPS[i]} ${PORTS[i]} >> $OUTPUT_DIR/ncat.txt 2>&1
 done
 
 echo "$(ncat --version 2>&1)
-$(sed '/nmap.org/d; s/^/  /g; s/^  ncat/\nncat/g' ncat.txt)" > ncat.txt
+$(sed '/nmap.org/d; s/^/  /g; s/^  ncat/\nncat/g' ncat.txt)" > $OUTPUT_DIR/ncat.txt
 
 ########
 # ???? #
