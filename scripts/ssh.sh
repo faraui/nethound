66c6c2d301c8e44cdcf1e307ea6d9dd196912352
Ruslan Abbas Al Faraui <rusya.abbas@gmail.com>
2024-06-05 15:58:06 +0200

    Update ssh.sh

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
-read OUTPUT_DIR
+read SCAN_DIR
 declare -a IPS PORTS
 while read LINE; do
   IFS=':' read -r IP PORT PROTOCOL <<< "$LINE"
   IPS+=("$IP")
   PORTS+=("$PORT")
-done < $OUTPUT_DIR/ports.txt
+done < $SCAN_DIR/ports.txt
 
 ########
 # ncat #
 ########
 for i in "${!IPS[@]}"; do
-  echo "ncat --verbose --nodns ${IPS[i]} ${PORTS[i]}" >> $OUTPUT_DIR/ncat.txt
-  ncat --verbose --nodns ${IPS[i]} ${PORTS[i]} >> $OUTPUT_DIR/ncat.txt 2>&1
+  echo "ncat --verbose --nodns ${IPS[i]} ${PORTS[i]}" >> $SCAN_DIR/ncat.txt
+  ncat --verbose --nodns ${IPS[i]} ${PORTS[i]} >> $SCAN_DIR/ncat.txt 2>&1
 done
 
 echo "$(ncat --version 2>&1)
-$(sed '/nmap.org/d; s/^/  /g; s/^  ncat/\nncat/g' $OUTPUT_DIR/ncat.txt)" > $OUTPUT_DIR/ncat.txt
+$(sed '/nmap.org/d; s/^/  /g; s/^  ncat/\nncat/g' $SCAN_DIR/ncat.txt)" > $SCAN_DIR/ncat.txt
 
 ########
 # ???? #
