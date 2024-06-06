35f2b0a2eed467162a6e1c11e030105f0f8e7620
Ruslan Abbas Al Faraui <rusya.abbas@gmail.com>
2024-06-06 15:02:08 +0200

    ++ ssh-keyscan

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -22,9 +22,9 @@ $(sed '/Ncat: [VC]/d; s/^/  /g; s/^  \$/\n\$/g' $SCAN_DIR/ncat.txt)" > $SCAN_DIR
 ###############
 # ssh-keyscan #
 ###############
-echo "ssh-keyscan by David Mazieres <dm@lcs.mit.edu> and Wayne Davison <wayned@users.sourceforge.net>" > ssh-keyscan.txt
+echo "ssh-keyscan by David Mazieres <dm@lcs.mit.edu> and Wayne Davison <wayned@users.sourceforge.net>" > $SCAN_DIR/ssh-keyscan.txt
 for i in "${!IPS[@]}"; do
   echo "
-\$ ssh-keyscan -p ${PORTS[i]} -T 10 -t dsa,ecdsa,rsa,ed25519 ${IPS[i]}" >> ssh-keyscan.txt
-  ssh-keyscan -p ${PORTS[i]} -T 10 -t dsa,ecdsa,rsa,ed25519 ${IPS[i]} 2>/dev/null | sed 's/^.*ssh-/  /g' >> ssh-keyscan.txt
+\$ ssh-keyscan -p ${PORTS[i]} -T 10 -t dsa,ecdsa,rsa,ed25519 ${IPS[i]}" >> $SCAN_DIR/ssh-keyscan.txt
+  ssh-keyscan -p ${PORTS[i]} -T 10 -t dsa,ecdsa,rsa,ed25519 ${IPS[i]} 2>/dev/null | sed 's/^.*ssh-/  /g' >> $SCAN_DIR/ssh-keyscan.txt
 done
