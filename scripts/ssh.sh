30bdf40814cc6d75a68015b0559d14f40588fe85
Ruslan Abbas Al Faraui <rusya.abbas@gmail.com>
2024-06-06 14:35:54 +0200

    + ssh-keyscan

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -12,13 +12,19 @@ done < $SCAN_DIR/ports.txt
 # ncat #
 ########
 for i in "${!IPS[@]}"; do
-  echo "ncat --verbose --nodns ${IPS[i]} ${PORTS[i]}" >> $SCAN_DIR/ncat.txt
-  ncat --verbose --nodns ${IPS[i]} ${PORTS[i]} >> $SCAN_DIR/ncat.txt 2>&1
+  echo "\$ ncat --verbose --nodns ${IPS[i]} ${PORTS[i]}" >> ncat.txt
+  ncat --verbose --nodns ${IPS[i]} ${PORTS[i]} >> ncat.txt 2>&1
 done
 
 echo "$(ncat --version 2>&1)
-$(sed '/Ncat: [VC]/d; s/^/  /g; s/^  ncat/\nncat/g' $SCAN_DIR/ncat.txt)" > $SCAN_DIR/ncat.txt
+$(sed '/Ncat: [VC]/d; s/^/  /g; s/^  \$/\n\$/g' $SCAN_DIR/ncat.txt)" > $SCAN_DIR/ncat.txt
 
-########
-# ???? #
-########
+###############
+# ssh-keyscan #
+###############
+echo "ssh-keyscan by David Mazieres <dm@lcs.mit.edu> and Wayne Davison <wayned@users.sourceforge.net>" > ssh-keyscan.txt
+for i in "${!IPS[@]}"; do
+  echo "
+\$ ssh-keyscan -p ${PORTS[i]} -T 10 -t dsa,ecdsa,rsa,ed25519 ${IPS[i]}" >> ssh-keyscan.txt
+  ssh-keyscan -p ${PORTS[i]} -T 10 -t dsa,ecdsa,rsa,ed25519 ${IPS[i]} 2>/dev/null | sed 's/^.*ssh-/  /g' >> ssh-keyscan.txt
+done
